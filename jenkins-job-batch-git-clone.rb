cbf8dde6fb9c87e9d924f8fa814cad1523a1da57
James Cuzella <james.cuzella@lyraphase.com>
2015-07-27 17:07:29 -0600

    Initial working MVP script
    Change-Id: Ica2ef986e0239e7be03cfab0e8cac3c714a67968

---
+++ jenkins-job-batch-git-clone.rb
@@ -0,0 +1,57 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require 'rubygems'
+require 'net/http'
+require 'jenkins_api_client'
+require 'addressable/uri'
+require 'pp'
+
+@jenkins_user  = ENV['JENKINS_USER'] || nil
+@jenkins_password = ENV['JENKINS_PASSWORD'] || nil
+@jenkins_host  = ENV['JENKINS_HOST'] || 'jenkins'
+@jenkins_port = ENV['JENKINS_PORT'] || '80'
+@jenkins_job_filter = ENV['JENKINS_JOB_FILTER'] || ''
+
+@jenkins_job_git_url_list = []
+
+if ENV['JENKINS_DEBUG']
+  printf "%42s = %s\n", "\033[1;32mJENKINS_HOST\033[0m", @jenkins_host
+  printf "%42s = %s\n", "\033[1;32mJENKINS_PORT\033[0m", @jenkins_port
+end
+
+class UnspecifiedParameters < RuntimeError
+end
+
+if @jenkins_user.nil? || @jenkins_password.nil?
+  printf "%s", "\033[1;31mERROR:\033[0m No JENKINS_USER or JENKINS_PASSWORD given."
+  raise UnspecifiedParameters, "Required Environment variables not set: No JENKINS_USER or JENKINS_PASSWORD given."
+end
+
+@client = JenkinsApi::Client.new(:server_ip => @jenkins_host, :server_port => @jenkins_port,
+         :username => @jenkins_user, :password => @jenkins_password)
+
+# For each job matching @jenkins_job_filter, search for git clone URI
+# If found, add to git_repo_url_list
+@client.job.list( @jenkins_job_filter ).each do |jenkins_job|
+  if ! @client.job.get_config( jenkins_job ).scan( /.*git.*/ ).empty?
+    config_xml = Nokogiri::XML( @client.job.get_config(jenkins_job) )
+    jenkins_job_git_url = config_xml.xpath("//scm[contains(@class,'git')]/userRemoteConfigs/hudson.plugins.git.UserRemoteConfig/url").text
+    if jenkins_job_git_url
+      @jenkins_job_git_url_list.push Addressable::URI.parse( jenkins_job_git_url )
+    end
+
+    if ENV['JENKINS_DEBUG']
+      puts "Jenkins job '#{jenkins_job}':\n\n"
+      puts config_xml.xpath("//scm[contains(@class,'git')]/userRemoteConfigs/hudson.plugins.git.UserRemoteConfig/url")
+      puts "\n\n"
+      ## If repoTag contains a literal '$', we want to know where this variable comes from
+      puts "Git URL:"
+      puts jenkins_job_git_url
+      puts "Current Build Number:"
+      puts @client.job.get_current_build_number( jenkins_job )
+    end
+
+  end
+end
+
